21ad39965d51b558913590ca708805bd8e03855b
Immanuel <33377846+GitImmanuel@users.noreply.github.com>
2017-11-17 20:07:24 +0100

    Update Wiskunde opracht 2.py

--- Wiskunde opracht 2.py
+++ Wiskunde opracht 2.py
@@ -1,9 +1,23 @@
+def neeeee():
+    from datetime import datetime
+    from datetime import time
+
+    current = datetime.now()
+    print('The current date and time is:', current)
+    print('')
+
+neeeee()
+
+print('Welcome!')
+print('')
+
 def ja():
     for a in range(1, 1000):
         for b in range(1, 1000):
             for c in range(1, 1000):
                 if a*a + b*b == c*c and a + b + c == 1000:
                     print(a, b, c)
+                    print('')
                     break
 
 ja()
